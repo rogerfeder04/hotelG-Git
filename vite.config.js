90eae3928241c5b68519fa93ceeeb24af1a60599
rogerfeder04 <rogerfeder0437@gmail.com>
2024-06-26 17:02:22 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/hotelG-Git/',
   plugins: [
     vue({
       template: { transformAssetUrls }
